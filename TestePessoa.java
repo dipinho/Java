d8c89ca592e788c37715b5bb27cc4f256ed27f7d
George de Pinho <49853721+dipinho@users.noreply.github.com>
2022-03-15 00:12:03 -0300

    Ex_Heranca

---
+++ TestePessoa.java
@@ -0,0 +1,15 @@
+package oo;
+
+public class TestePessoa {
+
+	public static void main(String[] args) {
+
+		Aluno aluno = new Aluno();
+		Professor professor = new Professor();
+		
+		aluno.setNome("George");
+		aluno.setTelefone("981878321");
+			
+	}
+
+}
